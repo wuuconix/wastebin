999701f6bddc6840f11eec79d52a7b6ab8293e02
wuuconix <1521900139@qq.com>
2022-06-03 13:31:02 +0800

    实现ctrl+s保存为纯文本

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const { writeFile, access } = require("fs/promises")
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post("/documents", (req, res) => {
+    const data = req.body.data
+    console.log(data)
+    const filename = uuidv4().replace(/-/g, "")
+    writeFile(`./data/${filename}`, data).then(() => {
+        res.json({success: "ok", filename})
+    })
+})
+
+app.get(/\/raw\/\w{32}/, (req, res) => { //获得text/plain接口
+    console.log()
+    const filename = req._parsedUrl.pathname.slice(5)
+    access(`./data/${filename}`).then(() => {
+        res.setHeader("Content-Type", "text/plain;charset=UTF-8")
+        res.sendFile(`${__dirname}/data/${filename}`)
+    }).catch(e => {
+        res.json({error: e.toString()})
+    })
+})
+
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.sendFile(`${__dirname}/static/index.html`)
+})
+
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("server is listenning in http://0.0.0.0:3000")
+})
