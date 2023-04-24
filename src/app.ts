8ddde4ba58bdf579f4f898ee8396815f63d0bac3
wuuconix <wuuconix@qq.com>
2023-04-24 13:14:28 +0800

    done /add

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 // @ts-ignore 这里由于没有使用--resolveJsonModule 编译会报错，但是不影响实际运行
 import config from "../config.json" assert { type: "json" }
 
-const filename = location.hash.slice(1) ?? ""
-console.log(filename)
-if (filename) {
-  const html = await (await fetch(`${config.api}/${filename}`)).text()
-  const div = document.querySelector("div.markdown-body")!
-  div.innerHTML = html
+window.addEventListener("hashchange", () => {
+  fetchHtml()
+})
+
+async function fetchHtml() {
+  const filename = location.hash.slice(1) ?? ""
+  console.log(filename)
+  if (filename) {
+    const html = await (await fetch(`${config.api}/parse/${filename}`)).text()
+    const div = document.querySelector("div.markdown-body")!
+    div.innerHTML = html
+  }
 }
+
+fetchHtml()
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import { marked } from "marked"
+// @ts-ignore 这个错误是@types/highlightjs 没有点导致的
 import hljs from "highlight.js"
 import express from "express"
-import { access, readFile } from "fs/promises"
+import { access, readFile, writeFile } from "fs/promises"
 import { dirname, normalize, extname } from 'path'
 import { fileURLToPath } from 'url'
+// @ts-ignore 导入json
+import config from "../config.json" assert { type: "json" }
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
-app.post("/add", (req, res) => {
+app.use(express.urlencoded({ extended: true }))   // 支持post body获取
 
+app.post("/add/:filename", async (req, res) => {
+  let { filename } = req.params               // e.g index.js
+  const ext = extname(filename)               // .js
+  const pre = filename.slice(0, -ext.length)  // index
+  filename = `${getTime()}_${pre}${ext}`      // 20230424124000_index.js
+  const path = normalize(`${__dirname}/../data/${filename}`)
+  try {
+    const { code } = req.body
+    await writeFile(path, code)
+    return res.json({ url: `${config.api}/raw/${filename}` })
+  } catch(e) {
+    console.log(e)
+    return res.json({ err: "failed to write" })
+  }
 })
 
 const mimeTypeMap = new Map([
@@ -27,6 +44,7 @@ app.get("/raw/:filename", async (req, res) => {
     res.setHeader("Content-Type", `${mimeType};charset=UTF-8`)
     return res.sendFile(path)
   } catch(e) {
+    console.log(e)
     return res.json({ err: "no such file" })
   }
 })
@@ -36,12 +54,16 @@ app.get("/parse/:filename", async (req, res) => {
   const path = normalize(`${__dirname}/../data/${filename}`)
   try {
     log(`/parse/${filename}`)
-    const md = await readFile(path, { encoding: "utf-8" })
+    let md = await readFile(path, { encoding: "utf-8" })
+    if (extname(filename) != ".md") {    // 如果不是md，则加上代码区域```变成md
+      md = "```" + (extname(filename).slice(1) ?? "") + "\n" + md + "\n" + "```"
+    }
     const html = parseMdToHtml(md)
     res.setHeader("Content-Type", "text/plain;charset=UTF-8")
     return res.send(html)
   } catch(e) {
-    return res.json(JSON.stringify(e))
+    console.log(e)
+    return res.json({ err: "no such file" })
   }
 })
 
@@ -62,4 +84,15 @@ function parseMdToHtml(md: string): string {
 
 function log(msg: string): void {
   console.log(`${new Date().toLocaleString()}:\n${msg}`)
+}
+
+function getTime(): string {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  const hour = String(date.getHours()).padStart(2, "0")
+  const min = String(date.getMinutes()).padStart(2, "0")
+  const sec = String(date.getSeconds()).padStart(2, "0")
+  return `${year}${month}${day}${hour}${min}${sec}`
 }
